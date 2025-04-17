fd1c29dd0a5b19bd08558dd74cb5d418a8ecdb34
cord-burmeister <cord.burmeister@live.de>
2025-04-17 10:35:36 +0200

    Some renaming

---
+++ bash/setup-ws-pit.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# bash won't source .bashrc from an interactive terminal unless I manually run bash from a terminal:
+# $ bash
+# or manually source it:
+source /home/$USER/.bashrc
+
+workspacename=m3_ws
+
+# First install required development tools
+sudo apt install python3-vcstool python3-colcon-common-extensions git wget -y
+sudo apt install libgflags-dev -y
+
+
+# Then create a new workspace and load the git repositories which are required.
+mkdir -p /home/$USER/$workspacename/src
+cd /home/$USER/$workspacename/src
+
+
+wget -O x3plus_description.yaml https://raw.githubusercontent.com/cord-burmeister/x3plus_description/refs/heads/main/x3plus_description.yaml
+vcs import < x3plus_description.yaml
+
+# Before building the workspace, you need to resolve the package dependencies. 
+# You may have all the dependencies already, but best practice is to check for 
+# dependencies every time you clone. You wouldn’t want a build to fail after 
+# a long wait only to realize that you have missing dependencies.
+
+cd /home/$USER/$workspacename
+if [ -e /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+    echo rosdep init  was already running.....
+else
+    sudo rosdep init
+fi
+
+rosdep update    
+echo rosdep install -r -y --from-path src --rosdistro $ROS_DISTRO 
+rosdep install -r -y --from-path src --rosdistro humble
+
+# cd /home/vagrant/$workspacename
+# colcon build
+
+# source /home/vagrant/$workspacename/install/setup.bash
+
+# Add some help finding errors in the logging 
+echo "export RCUTILS_COLORIZED_OUTPUT=1" >> /home/$USER/.bashrc 
+
+ # Add sourcing to your shell startup script
+echo "source /home/$USER/$workspacename/install/setup.bash" >> /home/$USER/.bashrc
+echo "cd /home/$USER/$workspacename" >> /home/$USER/.bashrc
+
+
+
