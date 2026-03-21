61ce9775879eedcdb6b8db9500d6e1e18191f224
Cord Burmeister <cord.burmeister@live.de>
2026-03-21 17:21:38 +0100

    Adjust the gazeb install script

--- bash/setup-ros2-gz-harmonic.sh
+++ bash/setup-ros2-gz-harmonic.sh
@@ -38,8 +38,8 @@
 ################################################################################
 
 sudo apt-get update
-sudo apt-get install lsb-release wget gnupg
-curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+sudo apt-get install -y lsb-release wget gnupg curl
+sudo curl -sSL https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
 sudo apt-get upgrade -y
